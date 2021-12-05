8349b230769702062f585c61ea225b29de33c153
L4ZZURPC\L4zzur <popovnikita01@gmail.com>
2021-12-05 17:20:20 +0200

    no message

--- 1.9_FFT.py
+++ 1.9_FFT.py
@@ -1,26 +1,81 @@
-from math import log
-n = int(input('Введите n: '))
-w = int(input('Введите w: '))
-a = input('Введите a: ')
+import math
+a = input('Введите коэффициенты: ')
+a = [int(i) for i in a.split()]
+m = int(input('Введите модуль: '))
 
-def fft(n, w, a):
-    m = int(w ** (n / 2) + 1)
-    a = [int(i) for i in a] # число в список его цифр
-    a.reverse()
+def gcd(a,b):
+    while b != 0:
+        a, b = b, a % b
+    return a
 
+def primRoots(modulo):
+    roots = []
+    required_set = set(num for num in range (1, modulo) if gcd(num, modulo) == 1)
 
-    if log(n, 2) % 1 != 0:
-        print('n не является степенью двойки')
+    for g in range(1, modulo):
+        actual_set = set(pow(g, powers) % modulo for powers in range (1, modulo))
+        if required_set == actual_set:
+            roots.append(g)           
+    return roots
+
+def bin_list(x, k):
+    y = []
+    for i in range(k):
+        y.append(0)
+    i = k - 1
+    while x > 0:
+        y[i] = x % 2
+        x = x // 2
+        i -= 1
+    return y
+
+def rev(x, k):
+    nums = bin_list(x, k)
+    nums.reverse()
+    nums = int(''.join(str(i) for i in nums), 2)
+    return nums
+
+def fft(a, m):
+    k = int(math.log(len(a), 2))
+    n = pow(2, k)
+
+    if primRoots(m) == []:
+        print('Примитивных корней не существует.')
         exit
     else:
-        k = int(log(n, 2))
+        w = int(input(f'Выберите корень из списка {primRoots(m)}: '))
+        if w not in primRoots(m):
+            print('Введён не примитивный корень')
+            exit
+    
+    S = []
+    R = []
+    for i in a:
+        R.append(i)
+        S.append(0)
+    
+    for l in range(k - 1, -1, -1):
+        for i in range(n):
+            S[i] = R[i]
+        for j in range(n):
+            i = bin_list(j, k)
+            i_0 = [x for x in i]
+            i_1 = [x for x in i]
 
-    s = []
-    r = []
+            i_0[l - 1] = 0
+            i_1[l - 1] = 1
+
+            i_0 = int(''.join(str(i) for i in i_0), 2)
+            i_1 = int(''.join(str(i) for i in i_1), 2)
+
+            deg = rev((j // pow(2, l)), k)
+
+            R[j] = (S[i_0] + pow(w, deg) * S[i_1]) % m
+
+    result = []
     for i in range(n):
-        s.append(0)
-        r.append(a[i])
+        result.append(R[rev(i, k)])
 
-    return k
-
-print(fft(n, w, a))
+    return result
+    
+print(f'Результат БПФ: {fft(a, m)}')
